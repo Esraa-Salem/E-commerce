6ddf9dd7f5bda6a002a3d78b634f26e576de1d76
Esraa-Salem <143455842+Esraa-Salem@users.noreply.github.com>
2024-02-27 23:35:38 +0200

    Update urls.py

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -14,29 +14,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Shopping API",
-        default_version="v1",
-        description="Graduation API v1",
-        terms_of_service="",
-    ),
-    public=True,
-)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('app1.urls')),
-    path(
-        "",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="v1-schema-swagger-ui",
-    ),
-]
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib import admin 
+from django.urls import path,include 
+from drf_yasg import openapi 
+from drf_yasg.views import get_schema_view 
+from app1 import views 
+from rest_framework.routers import DefaultRouter 
+router=DefaultRouter() 
+router.register('cart',views.CartViewSet,basename='cart') 
+router.register('cartitem',views.CartItemViewSet,basename='cartitem') 
+schema_view = get_schema_view( 
+    openapi.Info( 
+        title="Shopping API", 
+        default_version="v1", 
+        description="Graduation API v1", 
+        terms_of_service="", 
+    ), 
+    public=True, 
+) 
+ 
+urlpatterns = [ 
+    path('admin/', admin.site.urls), 
+    path('',include('app1.urls')), 
+    path('cart',include(router.urls)) , 
+    path( 
+        "", 
+        schema_view.with_ui("swagger", cache_timeout=0), 
+        name="v1-schema-swagger-ui", 
+    ), 
+    path( 
+        "", 
+        schema_view.with_ui("redoc", cache_timeout=0), 
+        name="schema-redoc", 
+    ), 
+] 
+from django.conf.urls.static import static 
+from django.conf import settings 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
